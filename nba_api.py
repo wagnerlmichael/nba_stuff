74fd7eaa17fa4f07659cf70a3be0e98355839dbe
wagnerlmichael <wagnerleemichael@gmail.com>
2023-03-08 12:59:51 -0600

    what

--- nba_api.py
+++ nba_api.py
@@ -2,6 +2,11 @@
 python3 -venv
 pip install -r requirements.txt
 deactivate
-"""
-print('cow')
 
+https://www.playingnumbers.com/2019/12/how-to-get-nba-data-using-the-nba_api-python-module-beginner/
+
+hash-r
+ipython
+"""
+from nba_api.stats.static import players
+player_dict = players.get_players()
