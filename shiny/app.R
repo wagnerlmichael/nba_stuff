7e8c207329dd158f020d17027926048847afb6df
wagnerlmichael <wagnerleemichael@gmail.com>
2023-03-09 20:28:13 -0600

    first graph draft fine

--- shiny/app.R
+++ shiny/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(tidyverse)
+library(shinyWidgets)
 
 
 # Rtsudio workaround to make sure path isn't hard-coded
@@ -18,22 +19,26 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
+      p('This graph displays how many times per season 
+        X number of points was scored in a game by a player.'),
       # Slider input for adjusting the number of points in the graph
+      chooseSliderSkin('Round'),
       sliderInput(inputId = "num_points", 
                   label = "Number of points:",
                   min = 0,
-                  max = 80, 
-                  value = 50),
+                  max = 70, 
+                  value = 50,
+                  ticks = F),
       sliderInput(inputId = "earliest_year", 
                   label = "Earliest Year",
                   min = 1940,
                   max = 2010, 
-                  value = 1980)
-    ),
+                  value = 1980,
+                  ticks = F,
+                  sep = '')),
     # Output for the graph
     mainPanel(
-      plotOutput(outputId = "graph")
-    )
+      plotOutput(outputId = "graph"))
   )
 )
 
@@ -50,15 +55,24 @@ server <- function(input, output, session) {
       ggplot(aes(x = season, y = number)) +
       geom_bar(stat = 'identity') +
       theme_minimal() +
+      labs(y = 'Points',
+           x = 'Season') +
       theme(axis.text.x = element_text(angle = 80,
-                                       hjust = 1))
+                                       hjust = 1,
+                                       size = 12),
+            axis.text.y = element_text(size = 15))
     
+    #theme(axis.text.x=element_text(size=15))
   })
-
-  # output$graph <- renderPlot({
-  #   ggplot(graph_data(), aes(x = x, y = y)) +
-  #     geom_line(color = input$color)
 }
 
 shinyApp(ui, server)
+
+
+# runApp(
+#   display.mode = "showcase",
+#   #display.mode = c("auto", "normal", "showcase"),
+#   test.mode = getOption("shiny.testmode", TRUE)
+# )
+#shinyApp(ui = ui, server = server, options=c(launch.browser = .rs.invokeShinyPaneViewer))
 
